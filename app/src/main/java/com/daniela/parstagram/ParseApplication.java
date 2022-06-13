4ea7ae3d9ccc8c317ea555c7237466bb4b6e6f67
Daniela Esparza Espinosa <desparza13@fb.com>
2022-06-12 20:28:19 -0700

    Parse added, login screen created, onClickListener on login checks if the user is correct by calling ParseUser's loginInBackground method and passing the entered username, password and a LogInCallback. If the login succeeded user navigates to MainActivity. Also, to implement user persistence if the user has already signed in and closed the app, the login screen is skipped and the user goes directly into the app

---
+++ app/src/main/java/com/daniela/parstagram/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.daniela.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application{
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("nCfnM6OHVwdc1bcjpxkUq6Q8gX26jDzU6KlqSsTj")
+                .clientKey("4wi1941qVtRi5xDCNKLcLD5iI0IrfSCoDhJETv86")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
