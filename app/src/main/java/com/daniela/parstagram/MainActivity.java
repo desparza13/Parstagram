2f2d73ea3d454c7f66c1fc6cc84d4b3f4b7f8d11
Daniela Esparza Espinosa <desparza13@fb.com>
2022-06-12 20:48:49 -0700

    Users can logout, added a logout button on the menu for a better UX, once you click it the ParseUser's logout method is called, the ParseUser is updated to null and you are redirected to the login menu, once you logout a toast is also launched for the user to know it has successfully logged out

--- app/src/main/java/com/daniela/parstagram/MainActivity.java
+++ app/src/main/java/com/daniela/parstagram/MainActivity.java
@@ -2,13 +2,52 @@ package com.daniela.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Menu (Used to create logout button)
+    //Inflate menu including button (items) if present
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.logout:{
+                Log.d(TAG, "logging out");
+                onLogoutButton();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onLogoutButton() {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+        Toast.makeText(MainActivity.this, "User logged out", Toast.LENGTH_SHORT).show();
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(i);
     }
 }
